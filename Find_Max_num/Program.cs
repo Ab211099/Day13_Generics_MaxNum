27fba15c9b46b47c7bce171b99babd91c4744f5a
Abhishek bhavekar <bhavekarabhi@gmail.com>
2022-09-11 20:19:58 +0530

    [Abhi]Added:

---
+++ Find_Max_num/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Find_Max_num
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10; int b = 50; int c = 30;
+
+            
+            Console.WriteLine("Max number is "+ Max_number<int>.printMaximumNumbers(a,b,c));
+        }
+    }
+}   
