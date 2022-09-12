fa819ee0ad0d9736113413efb5d250b7e720162e
Abhishek bhavekar <bhavekarabhi@gmail.com>
2022-09-12 13:19:53 +0530

    [Abhi]Added:Maximum string program

--- Find_Max_num/Program.cs
+++ Find_Max_num/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Find_Max_num
+﻿using System.Xml.Linq;
+
+namespace Find_Max_num
 {
     public class Program
     {
@@ -15,7 +17,23 @@
             float x = 10.2F; float y = 50.2F; float z = 30.3F;
 
 
-            Console.WriteLine("Max number is " + Max_number<float>.printMaximumNumbers(x, y, z));
+            Console.WriteLine("Max float number is " + Max_number<float>.printMaximumNumbers(x, y, z));
+
+            Console.WriteLine("_________________________________________________________");
+
+            
+            
+                 
+            Console.WriteLine("max string number is " + Find_Max_String<string>.printMaximumNumbers("a","ab","abc"));
+
+            Console.WriteLine("_________________________________________________________");
+
+            Console.WriteLine("########Refactor_1########");
+
+            Refactor1 obj = new Refactor1();
+
+            
+            Console.WriteLine("max refactor num is " + obj.Max<string>("abhi", "abhis", "abhishek"));       
         }
     }
 }   
